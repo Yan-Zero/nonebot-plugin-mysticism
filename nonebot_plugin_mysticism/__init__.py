152483744adfb4a7975c7ff8458a077f101aa85b
Yan <1964649083@qq.com>
2024-05-09 20:22:41 +0800

    add pyyaml

--- nonebot_plugin_mysticism/__init__.py
+++ nonebot_plugin_mysticism/__init__.py
@@ -2,7 +2,7 @@ from nonebot import get_driver
 from nonebot.plugin import PluginMetadata
 from .tarot import tarot
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __plugin_meta__ = PluginMetadata(
     name="神秘学助手",
     description="虽然现在只有塔罗派相关的工具喵",
@@ -10,7 +10,7 @@ __plugin_meta__ = PluginMetadata(
 /tarot [formations]
 /s.tarot [type]
 """,
-    homepage="https://github.com/Yan-Zero/nonebot_plugin_mysticism",
+    homepage="https://yan-zero.github.io/docs/coffee/mysticism",
     type="application",
     config=None,
     supported_adapters=["~onebot.v11"],
